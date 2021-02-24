a5a5f4937ba184b19c7e3dbeece2d9dfa47e1422
Micha Gorelick <mynameisfiber@gmail.com>
2021-02-24 14:17:18 +0100

    fix 'secret = None' case

--- aleph/manage.py
+++ aleph/manage.py
@@ -292,7 +292,7 @@ def sample_entities(secret, properties, types, schematas, limit, use_resevour, o
     collections = [
         collection
         for collection in Collection.all_authz(authz)
-        if collection.secret == secret and not collection.casefile
+        if (secret is None or collection.secret == secret) and not collection.casefile
     ]
     random.shuffle(collections)
     should = [{"exists": {"field": f"properties.{p}"}} for p in properties]
